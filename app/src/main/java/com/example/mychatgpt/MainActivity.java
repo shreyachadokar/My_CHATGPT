52899608d76b1bc5e897383f663ecb9415ae1326
shreyachadokar <97971183+shreyachadokar@users.noreply.github.com>
2024-06-09 21:18:02 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/mychatgpt/MainActivity.java
+++ app/src/main/java/com/example/mychatgpt/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","Your API key")
                 .post(body)
                 .build();
 
